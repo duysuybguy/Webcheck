d7df34fc560115d7b5a823608ecc984dc7c04dae
trananhx <84513904+trananhx@users.noreply.github.com>
2023-12-05 15:27:13 +0700

    Update checkAdmin.php

--- app/Http/Middleware/checkAdmin.php
+++ app/Http/Middleware/checkAdmin.php
@@ -16,7 +16,8 @@ class checkAdmin
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()&&Auth::user()->role==1) {
+        if (Auth::check()&&Auth::user()->role==1) 
+        {
             return $next($request);
         }
         return redirect('/');
