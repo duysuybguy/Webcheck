7a80080181f172135e3d57212cb09bc4acaabd48
trananhx <84513904+trananhx@users.noreply.github.com>
2023-12-06 10:51:46 +0700

    Update login.blade.php

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -76,32 +76,33 @@
 </head>
 
 <body>
-
     <!-- Main Wrapper -->
     <div class="main-wrapper">
+    <div class="header d-none">
+    
+    <!-- Header Menu -->
+    <ul class="nav nav-tabs user-menu">
+    <!-- Flag -->
+    <li class="nav-item">
+    <a href="#" id="dark-mode-toggle" class="dark-mode-toggle">
+    <i class="feather-sun light-mode"></i><i class="feather-moon dark-mode"></i>
+    </a>
+    </li>
+    <!-- /Flag -->
+    </ul>
+    <!-- Header Menu -->
 
-        <div class="header d-none">a
-            <!-- Header Menu -->
-            <ul class="nav nav-tabs user-menu">
-                <!-- Flag -->
-                <li class="nav-item">
-                    <a href="#" id="dark-mode-toggle" class="dark-mode-toggle">
-                        <i class="feather-sun light-mode"></i><i class="feather-moon dark-mode"></i>
-                    </a>
-                </li>
-                <!-- /Flag -->
-            </ul>
-            <!-- Header Menu -->
-        </div>
-        <div class="row"> 
-        
-        <!-- Login -->
-        <div id="app">
-        <user-login/>
-        </div>
-        <!-- /Login -->
-        </div>
     </div>
+
+    <div class="row"> 
+    <!-- Login -->
+    <div id="app">
+    <user-login/>
+    </div>
+    <!-- /Login -->
+    </div>
+    </div>
+    
     <!-- /Main Wrapper -->
 
     <!-- jQuery -->
